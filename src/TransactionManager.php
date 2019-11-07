368b69cb8f1ee76ebe85acdba6f40496bd65d3bb
Vesko Kenashkov <vesko@azonmedia.com>
2019-11-07 18:53:43 +0200

    wip

--- src/TransactionManager.php
+++ src/TransactionManager.php
@@ -2,7 +2,22 @@
 
 namespace Azonmedia\Transaction;
 
+use Azonmedia\Patterns\ScopeReference;
+
 class TransactionManager
 {
+    public function __construct()
+    {
 
+    }
+
+    public function begin(string $transction_type, ?ScopeReference $ScopeReference) : Transaction
+    {
+
+    }
+
+    public function commit()
+    {
+
+    }
 }
